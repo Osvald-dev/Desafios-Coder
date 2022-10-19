055c2bdfdd1919b6d4ba35ab7eb06e7e854f3ce1
Osvaldo Casabella <ocasabella98@gmail.com>
2022-10-19 11:18:14 -0300

    Actualizando proyecto Router and Views

--- desafio-router/src/index.js
+++ desafio-router/src/index.js
@@ -1,28 +1,29 @@
-import express, { json } from 'express';
-import { products } from './routers/productsRouter.js';
-import handlebars from 'express-handlebars';
 
-const PORT = 3000;
+import express from "express";
+import { productRouter } from "./routers/ProductRouter.js";
+import { ViewsRouter } from "./routers/ViewsRouter.js";
+import handlebars from "express-handlebars";
+
+const PORT = 8080;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+    defaultLayout: "main.hbs",
+  })
+);
 
-app.engine('hbs', handlebars.engine({
-    extname:'.hbs',
-    defaultLayout:'main.hbs',
-}))
-
-app.set('view engine', 'hbs');
-app.set('views', './views');
-
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 app.use("/", ViewsRouter);
-app.use('/api/productos', products);
+app.use("/api/productos", productRouter);
 
-
-
-
-app.listen(PORT,()=> console.log(`Runing in port: ${PORT}`));
-
+const server = app.listen(PORT, () =>
+  console.log("Running on port " + server.address().port)
+);
